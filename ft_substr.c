af5323ef944f2ffc0652790850639423b93eeff1
Gabriel Fernandes <gabrielsbfernandes@gmail.com>
2023-10-30 13:12:51 -0300

    ft_substr - Part2 functions

--- ft_substr.c
+++ ft_substr.c
@@ -1,14 +1,26 @@
 #include "libft.h"
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	char	*str;
 	char	*ptr;
+	unsigned int	s_size;
+	unsigned int	limit_index;
 
-	ptr = s;
-		while (ptr)
-		{
-			if(*ptr == start)
-			{
-				
-			}
-		}
+	limit_index = (start + len);
+	str = (char *)s;
+	s_size = (unsigned int)ft_strlen(str);
+	if (len + start > s_size)
+		ptr = (char *)malloc((s_size - start + 1) * sizeof(char));
+	else
+		ptr = (char *)malloc((len + 1) * sizeof(char));
+	if (!(ptr))
+		return (NULL);
+	while (ptr && limit_index > start)
+	{
+		*ptr = str[start];
+		start++;
+		ptr++;
+	}
+	*ptr = '\0';
+	return (ptr - len);
 }
