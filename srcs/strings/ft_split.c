78cde0bd7b610e889b519001c4d86273814ac91d
gabrfern <gsbfernandes@gmail.com>
2024-03-08 16:48:39 -0300

    inserting the correct 'include'

--- srcs/strings/ft_split.c
+++ srcs/strings/ft_split.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../includes/libft.h"
 
 static unsigned int	num_arr(const char *str, char c)
 {
